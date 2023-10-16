5a4c9c398c57fac2a1879d140df5f2126733e8c5
Ed <121364681+B-Edd@users.noreply.github.com>
2023-10-16 13:08:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,42 +21,109 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-const pillContainer = document.querySelector('.pill-container');
-const numberOfPills = 100; // Adjust the number of pills as desired
-
-const pillImages = [
-    'pill1.png',
-    'pill2.png',
-    'pill3.png'
-];
 
 
 
-function createFloatingPills() {
-    for (let i = 0; i < numberOfPills; i++) {
-        const pill = document.createElement('img');
-        pill.classList.add('pill');
+var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext('2d');
 
-        // Randomly select a pill image
-        const randomImageIndex = Math.floor(Math.random() * pillImages.length);
-        pill.src = 'images/' + pillImages[randomImageIndex];
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-    // Randomize initial positions
-        const randomLeft = Math.random() * 100;
-        const randomTop = Math.random() * 100;
-        pill.style.left = `${randomLeft}vw`;
-        pill.style.top = `${randomTop}vh`;
+var stars = [], // Array that contains the stars
+    FPS = 60, // Frames per second
+    x = 100, // Number of stars
+    mouse = {
+      x: 0,
+      y: 0
+    };  // mouse location
 
-        // Randomize animation duration and delay
-        const randomDuration = Math.random() * 3 + 2;
-        const randomDelay = Math.random() * 3;
-        pill.style.animationDuration = `${randomDuration}s`;
-        pill.style.animationDelay = `-${randomDelay}s`; // Use a negative delay for a staggered start
+// Push stars to array
 
-        pillContainer.appendChild(pill);
-    }
+for (var i = 0; i < x; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    radius: Math.random() * 1 + 1,
+    vx: Math.floor(Math.random() * 50) - 25,
+    vy: Math.floor(Math.random() * 50) - 25
+  });
 }
 
-window.onload = function() {
-    createFloatingPills(); // Call the function when the page is fully loaded
-};
+// Draw the scene
+
+function draw() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+
+  ctx.globalCompositeOperation = "lighter";
+
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var s = stars[i];
+
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.fillStyle = 'black';
+    ctx.stroke();
+  }
+
+  ctx.beginPath();
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var starI = stars[i];
+    ctx.moveTo(starI.x,starI.y);
+    if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
+    for (var j = 0, x = stars.length; j < x; j++) {
+      var starII = stars[j];
+      if(distance(starI, starII) < 150) {
+        //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
+        ctx.lineTo(starII.x,starII.y);
+      }
+    }
+  }
+  ctx.lineWidth = 0.2; //0.05
+  ctx.strokeStyle = 'black';
+  ctx.stroke();
+}
+
+function distance( point1, point2 ){
+  var xs = 0;
+  var ys = 0;
+
+  xs = point2.x - point1.x;
+  xs = xs * xs;
+
+  ys = point2.y - point1.y;
+  ys = ys * ys;
+
+  return Math.sqrt( xs + ys );
+}
+
+// Update star locations
+
+function update() {
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var s = stars[i];
+
+    s.x += s.vx / FPS;
+    s.y += s.vy / FPS;
+
+    if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
+    if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
+  }
+}
+
+canvas.addEventListener('mousemove', function(e){
+  mouse.x = e.clientX;
+  mouse.y = e.clientY - 50;
+});
+
+// Update and draw
+
+function tick() {
+  draw();
+  update();
+  requestAnimationFrame(tick);
+}
+
+tick();
